460faef0e9dacd14c592e44c4370bac13f5e4f8d
Daniil Petelin <daniilpetelin77@gmail.com>
2020-05-24 18:38:22 +0300

    add function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-let firstElem = document.querySelector('#firstElem').value;
-let secondElem = document.querySelector('#secondElem').value;
-let thirdElem = document.querySelector('#thirdElem').value;
+document.querySelector('#countInterpolation').onclick = () => {
+      let firstElem = +document.querySelector('#firstElem').value;
+      let secondElem = +document.querySelector('#secondElem').value;
+      let thirdElem = +document.querySelector('#thirdElem').value;
+      let firstAction = secondElem - firstElem;
+      let secondAction = firstAction * thirdElem;
+      let result = secondAction + firstElem;
+
+      document.querySelector('#result').innerHTML = result;
+};
